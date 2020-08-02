7bc0877cee7ecef3d038a46f94f118026cbedbe2
ffespechen <flavio.espechenieva@gmail.com>
2020-08-02 15:14:36 -0300

    Updates

---
+++ precache-manifest.0972178dd0f58b3df9af05d6a639cd83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26455b6ae544aab102725465eb9e7fd",
+    "url": "/gifExpertAppJS/index.html"
+  },
+  {
+    "revision": "a7e68afdbde70d604e6a",
+    "url": "/gifExpertAppJS/static/css/main.280c7c2a.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/gifExpertAppJS/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gifExpertAppJS/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7e68afdbde70d604e6a",
+    "url": "/gifExpertAppJS/static/js/main.6c46e72e.chunk.js"
+  },
+  {
+    "revision": "ed45b10420735ba50f8a",
+    "url": "/gifExpertAppJS/static/js/runtime-main.330b4345.js"
+  }
+]);
